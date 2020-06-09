285011645dc1983a66dc0cd3bec0bfe5f248ef99
w113083i <wiavre@gmail.com>
2020-06-09 21:29:45 +0200

    Revert "Camera rotates to player direction; Player has nav mesh agent for collision."
    This reverts commit 0801fc01f919118fa062e8fe6ce9313f43d5923a.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,13 +4,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityStandardAssets.Characters.ThirdPerson;
-using Cinemachine;
+
 public class PlayerMovement : MonoBehaviour
 {
 
     public ThirdPersonCharacter character;
-    private Rigidbody rb;
-    public CinemachineFreeLook camera;
+
     public float speed = 10f;
 
 
@@ -20,23 +19,21 @@ public class PlayerMovement : MonoBehaviour
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+
+
         character = this.GetComponent<ThirdPersonCharacter>();
-        Cursor.lockState = CursorLockMode.Locked;
+
     }
 
     void Update()
     {
-        //camera.m_XAxis.Value = transform.rotation.y;
-        //camera.m_XAxis
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
         Vector3 forward = transform.forward * vertical * speed * Time.deltaTime;
         Vector3 right = transform.right * horizontal * speed * Time.deltaTime;
 
-        //rb.AddForce(forward + right);
-        character.Move(forward + right, false, false);
+        character.Move(forward + right,false,false);
     }
 }
 
